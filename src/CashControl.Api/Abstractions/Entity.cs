9e694228d80d2d8a14909f329db97734cb8eafcb
Pablo Souza <contato@souzapablo.dev.br>
2025-08-07 21:16:48 -0300

    feat: create base entity and entity id

---
+++ src/CashControl.Api/Abstractions/Entity.cs
@@ -0,0 +1,46 @@
+namespace CashControl.Api.Abstractions;
+
+public abstract class Entity<TId> : IEquatable<Entity<TId>> where TId : EntityId<TId>
+{
+    protected Entity(TId id)
+    {
+        Id = id ?? throw new ArgumentNullException(nameof(id));
+    }
+
+    public TId Id { get; }
+    public DateTime CreatedAt { get; } = DateTime.UtcNow;
+    public DateTime? LastUpdate { get; private set; }
+    public bool IsActive { get; private set; } = true;
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null || obj is not Entity<TId> other)
+            return false;
+
+        if (ReferenceEquals(this, other))
+            return true;
+
+        return Id.Equals(other.Id);
+    }
+
+    public bool Equals(Entity<TId>? other) => Equals((object?)other);
+
+    public override int GetHashCode() => HashCode.Combine(GetType(), Id);
+
+    public static bool operator ==(Entity<TId>? left, Entity<TId>? right) =>
+        left is null && right is null || left is not null && left.Equals(right);
+
+    public static bool operator !=(Entity<TId>? left, Entity<TId>? right) => !(left == right);
+    protected void Update() => LastUpdate = DateTime.UtcNow;
+    public void Deactivate()
+    {
+        IsActive = false;
+        Update();
+    }
+
+    public void Activate()
+    {
+        IsActive = true;
+        Update();
+    }
+}
---
+++ src/CashControl.Api/Abstractions/EntityId.cs
@@ -0,0 +1,26 @@
+namespace CashControl.Api.Abstractions;
+
+public abstract class EntityId<T> : IEquatable<EntityId<T>> where T : EntityId<T>
+{
+    public Guid Value { get; }
+
+    protected EntityId(Guid value)
+    {
+        Value = value;
+    }
+    public static T New() => (T)Activator.CreateInstance(typeof(T), Guid.NewGuid())!;
+    public static T From(Guid value) => (T)Activator.CreateInstance(typeof(T), value)!;
+    
+    public override bool Equals(object? obj) => obj is EntityId<T> other && Equals(other);
+
+    public bool Equals(EntityId<T>? other) => other is not null && Value.Equals(other.Value);
+
+    public override int GetHashCode() => Value.GetHashCode();
+
+    public override string ToString() => Value.ToString();
+
+    public static bool operator ==(EntityId<T>? a, EntityId<T>? b) =>
+        a is null && b is null || a is not null && a.Equals(b);
+
+    public static bool operator !=(EntityId<T>? a, EntityId<T>? b) => !(a == b);
+}
