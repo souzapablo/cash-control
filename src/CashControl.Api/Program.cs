6ebc4cf52fd0e44c81b6eb91f3ea062666dd1e71
Pablo Souza <contato@souzapablo.dev.br>
2025-11-01 13:40:29 -0300

    feat(swagger): configure swagger

--- src/CashControl.Api/Program.cs
+++ src/CashControl.Api/Program.cs
@@ -3,10 +3,27 @@ using CashControl.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Cash Control API",
+        Version = "v1",
+        Description = "A simple personal finance app API"
+    });
+});
+
 var configuration = builder.Configuration;  
 builder.Services.AddInfrastructureModules(configuration);
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapEndpoints();
 
